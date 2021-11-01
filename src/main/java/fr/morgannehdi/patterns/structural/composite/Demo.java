b0d19525a36e1aba1db58eaeffc6f628a614e307
Morgan Nehdi <yannaro35@gmail.com>
2021-11-01 10:11:31 +0100

    Ajout du Composite

---
+++ src/main/java/fr/morgannehdi/patterns/structural/composite/Demo.java
@@ -0,0 +1,32 @@
+package fr.morgannehdi.patterns.structural.composite;
+
+import fr.morgannehdi.patterns.structural.composite.editor.ImageEditor;
+import fr.morgannehdi.patterns.structural.composite.shapes.Circle;
+import fr.morgannehdi.patterns.structural.composite.shapes.CompoundShape;
+import fr.morgannehdi.patterns.structural.composite.shapes.Dot;
+import fr.morgannehdi.patterns.structural.composite.shapes.Rectangle;
+
+import java.awt.*;
+
+public class Demo {
+    public static void main(String[] args) {
+        ImageEditor editor = new ImageEditor();
+
+        editor.loadShapes(
+                new Circle(10, 10, 10, Color.BLUE),
+
+                new CompoundShape(
+                        new Circle(110, 110, 50, Color.RED),
+                        new Dot(160, 160, Color.RED)
+                ),
+
+                new CompoundShape(
+                        new Rectangle(250, 250, 100, 100, Color.GREEN),
+                        new Dot(240, 240, Color.GREEN),
+                        new Dot(240, 360, Color.GREEN),
+                        new Dot(360, 360, Color.GREEN),
+                        new Dot(360, 240, Color.GREEN)
+                )
+        );
+    }
+}
---
+++ src/main/java/fr/morgannehdi/patterns/structural/composite/README.md
@@ -0,0 +1,17 @@
+# Composite
+
+## Intention
+
+**Composite** est un patron de conception structurel qui permet d’agencer les objets dans des arborescences afin de
+pouvoir traiter celles-ci comme des objets individuels.
+
+## Structure
+
+![Graph](Graph.png)
+
+## Structure particulière
+
+Dans cet exemple, le patron de conception **Composite** nous permet de gérer des imbrications de formes géométriques
+dans un éditeur graphique.
+
+![Graph2](Graph2.png)
